18364388c5ac49e7ee5fd597c287e0dfa6ef5039
Lee Shinhee <shlee2227@gmail.com>
2022-08-17 14:27:08 +0900

    [ADD]myapp/app.js의 signup 함수 typrorm 적용

--- myapp/app.js
+++ myapp/app.js
@@ -1,13 +1,15 @@
+const http = require("http");
 require("dotenv").config();
 const { DataSource } = require("typeorm");
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 const dotenv = require("dotenv");
-
+app.use(cors());
 dotenv.config();
+app.use(morgan("combined"));
+app.use(express.json());
 
 const myDataSource = new DataSource({
   type: process.env.TYPEORM_CONNECTION,
@@ -18,18 +20,66 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
 });
 
-app.use(cors());
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch(() => {
+    console.log("fail");
+  });
 
 app.get("/ping", function (req, res, next) {
   res.json({ message: "pong" });
 });
 
-app.listen(3000, function () {
-  console.log("server listening on port 3000");
+const signup = (req, res) => {
+  console.log("name, email, password");
+
+  //유저 정보 받아와서 유저 배열에 추가하고 성공시 성공메시지 반환
+  const { name, email, password } = req.body;
+  const queryRes = myDataSource.query(
+    `
+    INSERT INTO person (name, email,  password)
+    VALUES (?, ?, ?)
+  `,
+    [name, email, password]
+  );
+  queryRes
+    .then(() => {
+      res.status(201).json({ massage: "user created" });
+    })
+    .catch(() => {
+      res.status(500).json({ massage: "user create failed" });
+    });
+};
+
+app.post("/signup", (req, res) => {
+  //유저 정보 받아와서 유저 배열에 추가하고 성공시 성공메시지 반환
+  const { name, email, password } = req.body;
+  const queryRes = myDataSource.query(
+    `
+    INSERT INTO person (name, email,  password)
+    VALUES (?, ?, ?)
+  `,
+    [name, email, password]
+  );
+  queryRes
+    .then(() => {
+      res.status(201).json({ massage: "user created" });
+    })
+    .catch(() => {
+      res.status(500).json({ massage: "user create failed" });
+    });
 });
 
-app.use(morgan("combined"));
+// app.post("/login", login);
+// app.post("/createpost", createPost);
+// app.get("/postlist", postList);
+// app.patch("/editpost", editPost);
+// app.delete("/deletepost", deletePost);
+// app.post("/userpostlist", userPostList);
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
+app.listen(8000, function () {
+  console.log("server listening on port 8000");
 });
